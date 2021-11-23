2743c32385f06704421db689122b3f363da830db
Viktoriya <kitty_vikky@mail.ru>
2021-11-23 19:26:43 +0300

    изменен файл дизайна и добавлен случайный цвет для окружностей

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
+from UI import Ui_Form
 
 
-class MyWidget(QWidget):
+class MyWidget(Ui_Form, QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setFixedSize(400, 400)
         self.is_draw = False
         self.pushButton.clicked.connect(self.run)
@@ -28,7 +28,7 @@ class MyWidget(QWidget):
     def draw(self):
         a = randint(1, 100)
         x, y = randint(1, 400 - a), randint(1, 400 - a)
-        self.qp.setBrush(QColor('yellow'))
+        self.qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         self.qp.drawEllipse(x, y, a, a)
 
 
